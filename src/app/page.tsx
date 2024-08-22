fe600012d6938a30175d8bdf664a3aa467fd7d34
Felipe-Bignoto <felipebignoto@gmail.com>
2024-08-22 15:56:31 -0300

    feat: reset layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <div>teste</div>
+  return (
+    <div>
+      <h1>Desafio técnico</h1>
+      <div className="flex flex-col border-2 border-primary-dark bg-primary-light">
+        <button>Cadastrar novo usuário</button>
+        <button>Listar todos usuários</button>
+        <button>Procurar usuário pelo id</button>
+      </div>
+    </div>
+  )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,31 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        primary: {
+          light: '#A3B18A',
+          main: '#588157',
+          dark: '#3A5A40',
+        },
+        secondary: {
+          main: '#DAD7CD',
+        },
+        white: '#f7fafc',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
