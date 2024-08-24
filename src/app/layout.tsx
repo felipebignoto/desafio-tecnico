ccd95619e862587ab6c83ae2a0dbb332d42ba5e0
Felipe-Bignoto <felipebignoto@gmail.com>
2024-08-24 09:03:13 -0300

    feat: add shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import Link from 'next/link'
+import { Toaster } from '@/components/ui/toaster'
 
 export const metadata: Metadata = {
   title: 'Desafio técnico - BrandMonitor',
@@ -16,11 +17,15 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-secondary-main h-screen">
         <div className="flex items-center justify-center p-4">
-          <Link href="/" className="text-4xl underline text-primary-dark">
+          <Link
+            href="/"
+            className="text-4xl underline text-primary-dark hover:text-primary-main"
+          >
             Desafio técnico
           </Link>
         </div>
         {children}
+        <Toaster></Toaster>
       </body>
     </html>
   )
